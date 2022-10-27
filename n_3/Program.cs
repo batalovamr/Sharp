0085fcaed0bcc247488a912f1edff17129ed580c
batalovamr <batalovamr@yandex.ru>
2022-10-27 23:41:23 +0400

    add number 3

---
+++ n_3/Program.cs
@@ -0,0 +1,17 @@
+﻿int PrintAndGetValue(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int N = PrintAndGetValue("Введите число N: ");
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++) Console.Write(Math.Pow(i, 3) + "  ");
+}
+else if (N < 0)
+{
+    for (int i = N; i < 0; i++) Console.Write(Math.Pow(i, 3) + "  ");
+}
+else Console.WriteLine("Error");
