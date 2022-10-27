6d33ffd64531b11808d94a4f0ffb03cd1f799fe2
batalovamr <batalovamr@yandex.ru>
2022-10-27 23:12:06 +0400

    add number_1

---
+++ n_1/Program.cs
@@ -0,0 +1,13 @@
+﻿bool IsPalindrome(string number)
+{
+    if (number.Length == 5 && number[0] == number[4] && number[1] == number[3]) return true;
+    else return false;
+}
+
+
+Console.WriteLine("Введите положительное пятизначное число");
+string n = Console.ReadLine();
+bool nn = IsPalindrome(n);
+if (nn == true) Console.WriteLine("Это число палиндром");
+else Console.WriteLine("Это число не палиндром");
+
