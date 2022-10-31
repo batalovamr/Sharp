2e0f2fd77e2b695073c6febb6d3a4d058a0a5959
batalovamr <batalovamr@yandex.ru>
2022-10-31 21:17:46 +0400

    add number 2

---
+++ n_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int count = 0;
+int N = A;
+int B = 0;
+int sum = 0;
+while (N != 0)
+{
+    B = N % 10;
+    N = N / 10;
+    sum = sum + B;
+    count++;
+}
+Console.WriteLine(sum);
