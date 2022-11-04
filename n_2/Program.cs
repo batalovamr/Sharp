2b60d6ad53442f5770ff4de0641e36b697fea050
batalovamr <batalovamr@yandex.ru>
2022-11-04 15:56:01 +0400

    add number 1

---
+++ n_2/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] CreateRandomArray(int N, int min, int max)
+{
+    int[] array = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+int[] arr = CreateRandomArray(5, -100, 100);
+int sum = 0;
+int i = 0;
+while(i < arr.Length)
+{
+    sum = sum + arr[i];
+    i = i + 2;
+}
+string inputString = string.Join(",", arr);
+Console.WriteLine("Array = " + inputString);
+Console.WriteLine("Sum = " + sum);
+
