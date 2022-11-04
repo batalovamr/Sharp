5551b3896b8bfa1b5104d56a5374cf1890014ef1
batalovamr <batalovamr@yandex.ru>
2022-11-04 15:55:07 +0400

    add number 1

---
+++ n_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Ведите массив трехзначных чисел: ");
+string getArray = Console.ReadLine();
+string[] array = getArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+for (int i = 0; i < array.Length; i++)
+int[] arr = new int[array.Length];
+for (int i = 0; i < array.Length; i++) arr[i] = int.Parse(array[i]);
+Console.WriteLine(Count(arr));
