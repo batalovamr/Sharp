a15b8c211a8893809f94ae0155f106858367ca1c
batalovamr <batalovamr@yandex.ru>
2022-11-08 18:29:55 +0400

    add number 1

---
+++ n_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите массив чисел: ");
+string getArray = Console.ReadLine();
+string[] array = getArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] inputArray = new int[array.Length];
+for (int i = 0; i < array.Length; i++) inputArray[i] = int.Parse(array[i]);
+Console.WriteLine(Count(inputArray));
