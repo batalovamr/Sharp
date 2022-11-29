12d4c3fe6f9587293819bf5b3377c571e21e96d7
batalovamr <batalovamr@yandex.ru>
2022-11-29 18:20:44 +0400

    add number 5

---
+++ n_5/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+//Например, на выходе получается вот такой массив:
+//01 02 03 04
+//12 13 14 05
+//11 16 15 06
+//10 09 08 07
+
+int n = 4;
+int[,] matrix = new int[n, n];
+int temp = 1;
+int i = 0;
+int j = 0;
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+PrintArray(matrix);
+
+void PrintArray(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int col = 0; col < array.GetLength(1); col++)
+        {
+            Console.Write($"{array[row, col]} ");
+        }
+        Console.WriteLine();
+    }
+}
